83e6153f3ae0c809297a9d7814bea338fa8e6371
nelson kenzo tamashiro <nelsonkenzotamashiro@gmail.com>
2025-10-19 18:41:51 -0300

    chore: clerk env vars

--- src/env.js
+++ src/env.js
@@ -7,6 +7,8 @@ export const env = createEnv({
 	 * isn't built with invalid env vars.
 	 */
 	server: {
+		CLERK_SECRET_KEY: z.string(),
+
 		DATABASE_URL: z.string(),
 		NODE_ENV: z
 			.enum(["development", "test", "production"])
@@ -20,6 +22,7 @@ export const env = createEnv({
 	 */
 	client: {
 		// NEXT_PUBLIC_CLIENTVAR: z.string(),
+		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
 	},
 
 	/**
@@ -29,6 +32,10 @@ export const env = createEnv({
 	runtimeEnv: {
 		DATABASE_URL: process.env.DATABASE_URL,
 		NODE_ENV: process.env.NODE_ENV,
+
+		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+			process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+		CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
 	},
 	/**
 	 * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
